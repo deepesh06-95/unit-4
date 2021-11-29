b3242fc64448738119e9e8f378cbbf416717187e
deepesh06-95 <91532309+deepesh06-95@users.noreply.github.com>
2021-11-29 15:28:30 +0530

    Update app.js

--- evaluation29nov/app.js
+++ evaluation29nov/app.js
@@ -261,6 +261,46 @@ app.get("/jobs", async (req, res)=>{
     }
 });
 
+app.get("/jobs/city", async (req, res)=>{
+    try {
+        const jobs = await Job.find({$and: [{"city":{$eq: "bangalore"}},{"skill_id":{$eq: "61a49bb99d00c26d2d48bb2f"}}]}).populate({path: "skill_id", select: "skill_name"}).lean().exec();
+
+        return res.send({jobs});
+    } catch(e){
+        return res.status(500).json({message, status: "Failed"});
+    }
+});
+
+app.get("/jobs/work", async (req, res)=>{
+    try {
+        const jobs = await Job.find({"work_from_home":{$eq: "yes"}}).populate({path: "skill_id", select: "skill_name"}).lean().exec();
+
+        return res.send({jobs});
+    } catch(e){
+        return res.status(500).json({message, status: "Failed"});
+    }
+});
+
+app.get("/jobs/notice", async (req, res)=>{
+    try {
+        const jobs = await Job.find({"notice_period":{$eq: "2 months"}}).populate({path: "skill_id", select: "skill_name"}).lean().exec();
+
+        return res.send({jobs});
+    } catch(e){
+        return res.status(500).json({message, status: "Failed"});
+    }
+});
+
+app.get("/jobs/sort", async (req, res)=>{
+    try {
+        const jobs = await Job.find().populate({path: "skill_id", select: "skill_name"}).sort({"rating":1}).lean().exec();
+
+        return res.send({jobs});
+    } catch(e){
+        return res.status(500).json({message, status: "Failed"});
+    }
+});
+
 app.get("/jobs/:id", async (req, res)=>{
     try {
         const job = await Job.findById(req.params.id).lean().exec();
